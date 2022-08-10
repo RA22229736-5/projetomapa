1a93331b4c3e303e150d00e60fe2c7e8b430e96f
LitleKed <110494062+uniked@users.noreply.github.com>
2022-08-10 17:25:06 -0300

    update class footer

--- layout/footer.php
+++ layout/footer.php
@@ -1,12 +1,12 @@
 <footer>
     <ul class="footer-menu">
-        <li class="footer-menu-item nav-item" data-id="1"> <a href="#">Inscrever-se</a></li>
-        <li class="footer-menu-item nav-item" data-id="2"><a href="#">Blog</a></li>
-        <li class="footer-menu-item nav-item" data-id="3"><a href="#">Sobre</a></li>
-        <li class="footer-menu-item nav-item" data-id="4"><a href="#">Contato</a></li>
-        <li class="footer-menu-item nav-item" data-id="5"><a href="#">Redes Sociais</a></li>
-        <li class="footer-menu-item nav-item" data-id="6"><a href="#">Politica de Privacidade</a></li>
-        <li class="footer-menu-item nav-item" data-id="7"><a href="#">Termo de Uso</a></li>
+        <li> <a href="#">Inscrever-se</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="contato.php">Contato</a></li>
+        <li><a href="#">Redes Sociais</a></li>
+        <li><a href="#">Politica de Privacidade</a></li>
+        <li><a href="#">Termo de Uso</a></li>
     </ul>
     <hr />
     <p class="autoral"> &copy;2022 Desenvolvido por Kédma Costa - Acadêmica em SISIN - Unicesumar</p>
